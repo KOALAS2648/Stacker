370084bd1ec2a563d4f3e256bf825e15218cd260
KOALAS2648 <107459548+KOALAS2648@users.noreply.github.com>
2025-12-15 22:38:02 +0000

    done resolve

--- main.py
+++ main.py
@@ -5,7 +5,81 @@ def parse(idxInlist, section, times):
     return_code =[]
     for line in section:
         
-        if line[0] == "END":
+        if command_word == "END":
+            break
+        counter +=1
+
+    repeatedSection = section[1:counter]
+    return [i for i in repeatedSection*times]
+def main(file):
+    stacks = {}
+    file = open(file, "r")
+    file_data = file.readlines()
+    for idx, line in enumerate(file_data):
+        if "\n" in line:
+            file_data[idx] = line[:-1].split(" ")
+        else:
+            file_data[idx] = line.split(" ")
+    loop_words = ["LOOP", "END"]
+    for idx, line in enumerate(file_data):
+        #print(line)
+        loop_flag = False
+        command_word = command_word
+        if line == [""]:
+            continue
+        elif command_word in stacks and line[1].upper() == "STACK":
+            raise Exception(f"stack '{command_word}' cannot be created as stack '{command_word}' already exists")
+        if command_word not in loop_words:
+            if command_word not in stacks and line[1].upper() == "STACK":
+                stacks[command_word] = cs.Stack(int(line[-1]))
+                continue
+        if command_word in loop_words:
+            pass
+        else:
+            try:
+                stack_name = stacks[command_word]
+            except KeyError:
+                raise Exception(f"stack {command_word} does not exist")
+        
+        if command_word not in loop_words:
+            n = line[1]
+            match n.upper():
+                case "PUSH":
+                    stack_name.push(line[-1])
+                case "POP":
+                    stack_name.pop()
+                case "PEEK":
+                    print(stack_name.peek())
+                case "SIZE":
+                    print(stack_name.size())
+                case "ISEMPTY":
+                    print(stack_name.isEmpty())
+                case "PRINT":
+                    print(stack_name.data)
+                case "ISFULL":
+                    print(stack_name.isFull())
+                case _:
+                    raise Exception(f"Invalid command: {line[1]}")
+
+        elif command_word == "LOOP":
+            dataToInsert = parse(idx, file_data[idx:], int(line[1]))
+            file_data.pop(idx)
+            for i in dataToInsert:
+                file_data.insert(idx, i)
+            
+
+
+if __name__ == "__main__":
+    main("code.txt")
+
+import classStack as cs
+
+def parse(idxInlist, section, times):
+    counter = 0
+    return_code =[]
+    for line in section:
+        
+        if command_word == "END":
             break
         counter +=1
 
